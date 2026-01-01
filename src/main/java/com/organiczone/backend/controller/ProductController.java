6e9372530cd036929a64b7c5c37c9e7fcc87268f
Lavanya Medipudi <medipudilavanya@gmail.com>
2026-01-01 21:17:02 +0530

    Add Product REST controller

---
+++ src/main/java/com/organiczone/backend/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.organiczone.backend.controller;
+
+import com.organiczone.backend.model.Product;
+import com.organiczone.backend.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
