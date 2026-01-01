047afafbb1ed6649f858e2e16f1f89ab38a1451e
Lavanya Medipudi <medipudilavanya@gmail.com>
2026-01-01 18:44:24 +0530

    Add Spring Boot main application class

---
+++ src/main/java/com/organiczone/backend/OrganicZoneApplication.java
@@ -0,0 +1,12 @@
+package com.organiczone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganicZoneApplication.class, args);
+    }
+}
