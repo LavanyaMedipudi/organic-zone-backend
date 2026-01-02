e9b050fea6b02a964445743f0fcd62a9c8c3e58f
Lavanya Medipudi <medipudilavanya@gmail.com>
2026-01-02 12:30:01 +0530

    Add Product controller

--- src/main/java/com/organiczone/backend/controller/ProductController.java
+++ src/main/java/com/organiczone/backend/controller/ProductController.java
@@ -1,28 +1,64 @@
-package com.organiczone.backend.controller;
-
-import com.organiczone.backend.model.Product;
-import com.organiczone.backend.service.ProductService;
-import org.springframework.web.bind.annotation.*;
+package com.college.organiczone_backend.controller;
 
 import java.util.List;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.college.organiczone_backend.dto.ProductRequestDTO;
+import com.college.organiczone_backend.model.Product;
+import com.college.organiczone_backend.repository.ProductRepository;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/products")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class ProductController {
 
-    private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    @Autowired
+    private ProductRepository productRepository;
 
     @PostMapping
-    public Product addProduct(@RequestBody Product product) {
-        return productService.saveProduct(product);
+    public Product addProduct(@Valid @RequestBody ProductRequestDTO dto) {
+
+    return productRepository
+        .findByNameAndCategoryAndFarmerEmail(
+            dto.getName(),
+            dto.getCategory(),
+            dto.getFarmerEmail()
+        )
+        .map(existingProduct -> {
+            // Increase quantity
+            existingProduct.setQuantity(
+                existingProduct.getQuantity() + dto.getQuantity()
+            );
+
+            // update price
+            existingProduct.setPrice(dto.getPrice());
+
+            return productRepository.save(existingProduct);
+        })
+        .orElseGet(() -> {
+            // Create new product
+            Product product = new Product();
+            product.setName(dto.getName());
+            product.setCategory(dto.getCategory());
+            product.setPrice(dto.getPrice());
+            product.setQuantity(dto.getQuantity());
+            product.setFarmerEmail(dto.getFarmerEmail());
+
+            return productRepository.save(product);
+        });
     }
 
     @GetMapping
     public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
     }
 }
+
+
